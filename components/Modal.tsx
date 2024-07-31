cd04c8af6f7b3142a157cdf2525ecca81fe01fcc
Maxim <mnosov622@gmail.com>
2024-07-31 12:12:26 -0400

    add animation

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -47,7 +47,7 @@ const Modal = ({ productId }: Props) => {
 				</Dialog.Trigger>
 				<Dialog.Portal>
 					<Dialog.Overlay className="fixed inset-0 bg-black opacity-50" />
-					<Dialog.Content className="dialog-content fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+					<Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
 						<div className="flex flex-col">
 							<div className="flex justify-between">
 								<div className="p-3 border border-gray-200 rounded-10">
